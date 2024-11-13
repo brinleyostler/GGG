9f9b2a9ac9fb61d1bccd6622718142e1e4e17209
Brinley Ostler <brinley.ostler@gmail.com>
2024-11-13 09:35:30 -0700

    add Naive Bayes model

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -14,7 +14,6 @@ ggg_test = vroom("./test.csv")
 ggg_train = vroom("./train.csv")
 
 ## CLEAN THE DATA
-ggg_train$color = factor(ggg_train$color)
 ggg_train$type = factor(ggg_train$type)
 
 
@@ -22,30 +21,28 @@ ggg_train$type = factor(ggg_train$type)
 
 
 #### RECIPE ####
-### BOOSTED TREES ####
+### NAIVE BAYES ####
 ggg_recipe <- recipe(type~., data=ggg_train) %>% 
-  step_dummy(color) %>% 
-  step_range(all_numeric_predictors())
+  step_mutate(id, features = id) %>% 
+  step_mutate(color = as.factor(color))
 
 ggg_prepped = prep(ggg_recipe)
 baked <- bake(ggg_prepped, new_data = ggg_train)
 
 ### MODEL ####
-boost_model <- boost_tree(tree_depth=tune(),
-                          trees=tune(),
-                          learn_rate=tune()) %>%
-  set_engine("lightgbm") %>% #or "xgboost" but lightgbm is faster
+nb_model <- naive_Bayes(Laplace=tune(),
+                        smoothness=tune()) %>%  
+  set_engine("naivebayes") %>%
   set_mode("classification")
 
 ## Put into a workflow
-boost_workflow <- workflow() %>% 
+nb_workflow <- workflow() %>% 
   add_recipe(ggg_recipe) %>% 
-  add_model(boost_model)
+  add_model(nb_model)
 
 ## Grid of values to tune over
-tuning_grid <- grid_regular(tree_depth(),
-                            trees(),
-                            learn_rate(),
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
                             levels=5)
 
 
@@ -54,7 +51,7 @@ tuning_grid <- grid_regular(tree_depth(),
 folds <- vfold_cv(ggg_train, v=5, repeats=1)
 
 ## Run the CV
-tuned_boost <- boost_workflow %>% 
+tuned_nb <- nb_workflow %>% 
   tune_grid(resamples=folds,
             grid=tuning_grid,
             metrics=metric_set(roc_auc, f_meas, sens, recall,
@@ -63,11 +60,11 @@ tuned_boost <- boost_workflow %>%
 
 
 ## Find best tuning parameters
-best_tune <- tuned_boost %>% 
+best_tune <- tuned_nb %>% 
   select_best(metric="roc_auc")
 
 #### Finalize the workflow and fit it ####
-final_wf <- boost_workflow %>% 
+final_wf <- nb_workflow %>% 
   finalize_workflow(best_tune) %>% 
   fit(data=ggg_train)
 
@@ -82,7 +79,7 @@ recipe_kaggle_submission <- ggg_preds %>%
   select(id, type)
 
 ## Write out file
-vroom_write(x=recipe_kaggle_submission, file="./BoostPreds.csv", delim=",")
+vroom_write(x=recipe_kaggle_submission, file="./NBPreds.csv", delim=",")
 
 
 
